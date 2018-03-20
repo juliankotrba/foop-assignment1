be6908d212146771ce85aebbd9797e500fc339ec
Julian Kotrba <me@juliankotrba.xyz>
2018-03-20 16:39:32 +0100

    Create possibly needed DTO

---
+++ botracer/dto/src/main/java/dto/Mark.java
@@ -0,0 +1,15 @@
+package dto;
+
+import java.io.Serializable;
+
+/**
+ * Mark DTO
+ *
+ * @author  Julian Kotrba
+ */
+public class Mark implements Serializable {
+
+    private Position position;
+
+    private static final long serialVersionUID = 1L;
+}
