3417cb3a69faea525304863395d7312fd22e383a
Julian Kotrba <me@juliankotrba.xyz>
2018-03-20 16:39:01 +0100

    Create possibly needed DTO

---
+++ botracer/dto/src/main/java/dto/Message.java
@@ -0,0 +1,12 @@
+package dto;
+
+import java.io.Serializable;
+
+/**
+ * Message DTO
+ *
+ * @author  Julian Kotrba
+ */
+public class Message implements Serializable {
+    private static final long serialVersionUID = 1L;
+}
