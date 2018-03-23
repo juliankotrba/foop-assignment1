af17c4182e5bc2a62574b40109fbbb615836c64d
Julian Kotrba <me@juliankotrba.xyz>
2018-03-23 09:32:11 +0100

    Add possibly needed interface for solving the maze

---
+++ botracer/dto/src/main/java/dto/algorithms/MazeSolverStrategy.java
@@ -0,0 +1,17 @@
+package dto.algorithms;
+
+import dto.Grid;
+import dto.Position;
+import dto.Tile;
+
+/**
+ * Interface for calculating the next position of a bot
+ *
+ * @author Julian Kotrba
+ */
+public interface MazeSolverStrategy {
+
+    Position nextPosition(Position position, Grid<Tile> grid);
+
+    void reset();
+}
