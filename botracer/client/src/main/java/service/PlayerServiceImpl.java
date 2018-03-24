0dda7377deafb297d75b15b4f7b7dce33e002dd2
Julian Kotrba <me@juliankotrba.xyz>
2018-03-24 17:56:58 +0100

    Implement player service

---
+++ botracer/client/src/main/java/service/PlayerServiceImpl.java
@@ -0,0 +1,32 @@
+package service;
+
+import connection.Connection;
+import connection.OnMessageReceivedListener;
+import dto.messages.s2c.PlayersChangedMessage;
+import exception.service.ServiceException;
+
+/**
+ * Implementation of the player service
+ *
+ * @author Julian Kotrba
+ */
+public class PlayerServiceImpl implements PlayerService {
+
+    private Connection connection;
+
+    public PlayerServiceImpl(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public void registerForPlayerUpdates(OnMessageReceivedListener<PlayersChangedMessage> playerUpdate) throws ServiceException {
+
+        this.connection.setMessageListener(message -> {
+            if (message instanceof PlayersChangedMessage) {
+                if (playerUpdate != null) {
+                    playerUpdate.onMessageReceived((PlayersChangedMessage) message);
+                }
+            }
+        });
+    }
+}
