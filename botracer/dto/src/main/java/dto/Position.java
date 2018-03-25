ebfca3bd85fd2368865e79c2ea0ba6152ce0e23c
Julian Kotrba <me@juliankotrba.xyz>
2018-03-25 12:29:43 +0200

    Add equals/hash code

--- botracer/dto/src/main/java/dto/Position.java
+++ botracer/dto/src/main/java/dto/Position.java
@@ -1,6 +1,7 @@
 package dto;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * Position DTO
@@ -25,5 +26,20 @@ public class Position implements Serializable {
 
     public int getY() {
         return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return x == position.x &&
+                y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(x, y);
     }
 }
